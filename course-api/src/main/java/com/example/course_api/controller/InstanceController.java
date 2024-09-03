b8a5a2c6e5d9ea124856d85842f7513e0d592d2e
vishakha <vishakhakasture302002@gmail.com>
2024-09-03 21:12:38 +0530

    REST API

--- course-api/src/main/java/com/example/course_api/controller/InstanceController.java
+++ course-api/src/main/java/com/example/course_api/controller/InstanceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courseinstance")
+@RequestMapping("/instance")
 public class InstanceController {
     @Autowired
     private InstanceService instanceService;
